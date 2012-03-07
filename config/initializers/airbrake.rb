3399d268a07df5d1cce487124d5909524a52c2d9
Jessy Bernal <jessy.benal@gmail.com>
2012-03-07 17:06:43 +0100

    moche

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,5 +1,5 @@
-return unless Rails.env.prod?
-
-Airbrake.configure do |config|
-  config.api_key = '54713a9e51c89e3667a1239fa002b281'
+if Rails.env.prod?
+  Airbrake.configure do |config|
+    config.api_key = '54713a9e51c89e3667a1239fa002b281'
+  end
 end
