b7e3124addae55521b77eff28fac8bc1863e9131
Ivan Schneider <git@ivanschneider.fr>
2012-03-14 18:30:43 +0100

    fix for sso login with heroku

--- app/controllers/heroku/resources_controller.rb
+++ app/controllers/heroku/resources_controller.rb
@@ -1,6 +1,6 @@
 class Heroku::ResourcesController < ApplicationController
 
-  skip_filter :connect_to_db
+  skip_filter :connect_to_db, :require_authentication
   before_filter :basic_auth, :except => :sso_login
   
   def create
