14d5abd12e0698d7af9d8105c7297da5d72239a7
Ivan Schneider <git@ivanschneider.fr>
2012-04-05 16:14:32 +0200

    exclude date and date time columns from possible enum columns typo on homepage

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -6,13 +6,13 @@ class SettingsController < ApplicationController
       param_key = "#{type}_columns".to_sym
       settings.columns[type] = params[param_key].delete_if {|e|e.empty?} if params.has_key? param_key
     end
+    [:enum_values, :validations].each do |setting|
+      settings.send "#{setting}=", params[setting].delete_if {|e|e.empty?} if params.has_key? setting
+    end
     settings.filters = params[:filters].values if params.has_key?(:filters)
     settings.per_page = params[:per_page] if params[:per_page]
     settings.label_column = params[:label_column] if params[:label_column]
     settings.default_order = params[:default_order].join(' ') if params[:default_order].present?
-    [:enum_values, :validations].each do |setting|
-      settings.send "#{setting}=", params[setting].delete_if {|e|e.empty?} if params.has_key? setting
-    end
     settings.save
     redirect_to :back, flash: {success: 'Settings successfully saved.'}
   end
