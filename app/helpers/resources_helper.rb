d3968944295ec8af919f4470778c757b8b5b0bc9
Jessy Bernal <jessy.benal@gmail.com>
2012-04-06 12:55:18 +0200

    remaniement du page suivant / precedent pour faire plus gmail interface

--- app/helpers/resources_helper.rb
+++ app/helpers/resources_helper.rb
@@ -10,4 +10,22 @@ module ResourcesHelper
     res << (link_to key.humanize, params.merge(order:order), style:style)
   end
 
+  def page_entries_info(collection, options = {})
+    entry_name = options[:entry_name] || (collection.empty?? 'entry' : collection.first.class.name.underscore.sub('_', ' '))
+    if collection.num_pages < 2
+      case collection.total_count
+      when 0; "0 #{entry_name.pluralize}"
+      when 1; "<b>1</b> #{entry_name}"
+      else;   "<b>#{collection.total_count}</b> #{entry_name.pluralize}"
+      end
+    else
+      offset = (collection.current_page - 1) * collection.limit_value
+      %{<b>%d&nbsp;-&nbsp;%d</b> of <b>%d</b>} % [
+        offset + 1,
+        offset + collection.count,
+        collection.total_count
+      ]
+    end
+  end
+
 end
