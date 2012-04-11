16767d2383f71ffc3004363e4053f9c9c8592f94
Jessy Bernal <jessy.benal@gmail.com>
2012-04-11 18:48:18 +0200

    don't see the home if you're signed in

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,33 +1,34 @@
 class DocsController < ApplicationController
-  
+
   skip_filter :require_authentication
   skip_filter :connect_to_db, :unless => :valid_db_url?
-  
+
   def index
   end
 
   def homepage
+    redirect_to dashboard_url and return if session[:account]
     render :layout => 'homepage'
   end
-  
+
   def show
     render params[:id]
   end
-  
+
   def start_demo
     session[:account_before_demo] = session[:account] unless session[:account] == ENV['DEMO_ACCOUNT_ID']
     session[:account] = ENV['DEMO_ACCOUNT_ID']
     redirect_to dashboard_url
   end
-  
+
   def stop_demo
     session[:account] = session[:account_before_demo]
     redirect_to dashboard_url
   end
-  
+
   private
   def valid_db_url?
     session[:account] && current_account.valid_db_url?
   end
-  
+
 end
