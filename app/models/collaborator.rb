0b2d0789a681d3d480275a5e29001b1d0b74c50c
Ivan Schneider <git@ivanschneider.fr>
2012-04-19 22:43:36 +0200

    mail collaborators when added

--- app/models/collaborator.rb
+++ app/models/collaborator.rb
@@ -5,9 +5,15 @@ class Collaborator < ActiveRecord::Base
   validates :email, presence: true, 
                     format: {with: /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i}
   before_create :match_with_existing_user
+  after_create :mail_collaborator
   
   def match_with_existing_user
     user = User.where(email: email).first
     self.user_id = user.id if user
   end
+  
+  def mail_collaborator
+    CollaboratorMailer.notify_collaboration(self).deliver
+  end
+  
 end
