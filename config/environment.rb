492825e33d93e9390dc2e56e7a21e2b7d6baedc1
Ivan Schneider <git@ivanschneider.fr>
2012-06-22 12:07:20 +0200

    trying to debug crash on Heroku with latest Rails

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-
+puts File.read(File.expand_path('../database.yml', __FILE__))
 # Initialize the rails application
 MtCrap::Application.initialize!
