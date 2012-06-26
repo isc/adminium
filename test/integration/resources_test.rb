bd2d0690d098c0152ebfe7db77ccf7bdce46f86d
Ivan Schneider <git@ivanschneider.fr>
2012-06-26 18:17:50 +0200

    test for custom column belongs_to

--- test/integration/resources_test.rb
+++ test/integration/resources_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class ResourcesTest < ActionDispatch::IntegrationTest
-  
+
   test "index on resources for users table" do
     login
     visit resources_path(:users)
@@ -11,25 +11,32 @@ class ResourcesTest < ActionDispatch::IntegrationTest
     click_button 'Save settings'
     assert !page.has_css?('th a', :text => 'First name')
   end
-  
+
   test "creating a comment (polymorphic belongs_to)" do
     login
     visit resources_path(:comments)
     link = find('a[title="Create a new row"]')
     link.click()
   end
-  
+
   test "save and create another" do
     login
     visit new_resource_path(:users)
     # save_and_open_page
   end
-  
+
   test "custom column has_many" do
     login
-    Settings::Base.any_instance.stubs(:columns).returns :listing => ['has_many/comments'], :serialized => [], :search => []
+    Settings::Base.any_instance.stubs(:columns).returns listing: ['has_many/comments'], serialized: [], search: []
     visit resources_path(:users)
     assert page.has_css?('td.hasmany a', :text => '2')
   end
   
+  test "custom column belongs_to" do
+    login
+    Settings::Base.any_instance.stubs(:columns).returns listing: ['user.pseudo'], serialized: [], search: []
+    visit resources_path(:comments)
+    assert page.has_css?('td', :text => 'bob')
+  end
+
 end
