0ffe0d09154efe73bfe3b0c6acab278eaff27fc9
Ivan Schneider <git@ivanschneider.fr>
2012-09-28 00:39:34 +0200

    fixes for CSV export (find in batches, file downloading, correct includes)

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -40,6 +40,9 @@ class ResourcesController < ApplicationController
           total_count: @items.total_count
         }
       end
+      format.csv do
+        send_data generate_csv, type: 'text/csv'
+      end
     end
   end
 
@@ -200,7 +203,8 @@ class ResourcesController < ApplicationController
   end
 
   def apply_includes
-    assocs = clazz.settings.columns[:listing].find_all {|c| c.include? '.'}.map {|c| c.split('.').first}
+    settings_type = request.format.to_s == 'text/csv' ? :export : :listing
+    assocs = clazz.settings.columns[settings_type].find_all {|c| c.include? '.'}.map {|c| c.split('.').first}
     assocs.each do |assoc|
       @items = @items.includes(assoc.to_sym)
     end
@@ -285,6 +289,33 @@ class ResourcesController < ApplicationController
       object[key] = nil
     end
     object
+  end
+  
+  def generate_csv
+    keys = clazz.settings.columns[:export]
+    options = {col_sep: clazz.settings.export_col_sep}
+    out = if clazz.settings.export_skip_header
+      ''
+    else
+      keys.map { |k| clazz.column_display_name k }.to_csv(options)
+    end
+    @items.find_each do |item|
+      out << keys.map do |key|
+        if key.include? "."
+          parts = key.split('.')
+          pitem = item.send(parts.first)
+          pitem[parts.second] if pitem
+        elsif key.starts_with? 'has_many/'
+          key = key.gsub 'has_many/', ''
+          foreign_key_name = item.class.original_name.underscore + '_id'
+          foreign_key_value = item[item.class.primary_key]
+          item.class.generic.table(key).where(foreign_key_name => foreign_key_value).count
+        else
+          item[key]
+        end
+      end.to_csv(options)
+    end
+    out
   end
 
 end
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ if defined?(Bundler)
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
+require 'csv'
 
 module MtCrap
   class Application < Rails::Application
