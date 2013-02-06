10d91e05ac63949580956131b58878430358edca
Ivan Schneider <git@ivanschneider.fr>
2013-02-06 09:57:02 -0300

    upgrade link seems to be deprecated by the new marketplace, replaced by a simple link to the add-on page

--- app/models/account.rb
+++ app/models/account.rb
@@ -65,7 +65,8 @@ class Account < ActiveRecord::Base
   end
   
   def upgrade_link
-    "https://api.heroku.com/v3/resources/adminium+#{{Plan::PET_PROJECT => Plan::STARTUP, Plan::STARTUP => Plan::ENTERPRISE}[plan]}?selected=#{name}"
+    # "https://api.heroku.com/v3/resources/adminium+#{{Plan::PET_PROJECT => Plan::STARTUP, Plan::STARTUP => Plan::ENTERPRISE}[plan]}?selected=#{name}"
+    'https://addons.heroku.com/adminium'
   end
   
   private
