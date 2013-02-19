62bc74f9c0b76495d531cd30e80962fcbc9241d8
Jessy Bernal <jessy.bernal@gmail.com>
2013-02-19 11:33:04 +0100

    fix again

--- test/functional/resources_controller_test.rb
+++ test/functional/resources_controller_test.rb
@@ -87,6 +87,13 @@ class ResourcesControllerTest < ActionController::TestCase
     assert_equal @records.map(&:id), assigns[:record_ids].map(&:to_i)
   end
 
+  def test_search_for_association_input
+    get :search, :table => 'users', :search => 'Loulou'
+    data = JSON.parse(@response.body)
+    assert_equal 1, data.length
+    assert_equal "Loulou", data.first['pseudo']
+  end
+
   private
   def assert_asearch name, pseudos
     get :index, :table => 'users', :asearch => name, :order => 'pseudo'
