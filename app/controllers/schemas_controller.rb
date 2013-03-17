312fe8458ae5945728258886b7514c6ad4d99a9d
Ivan Schneider <git@ivanschneider.fr>
2013-03-17 16:38:30 +0100

    minor adjustments on schemas#show

--- app/controllers/schemas_controller.rb
+++ app/controllers/schemas_controller.rb
@@ -1,9 +1,9 @@
 class SchemasController < ApplicationController
 
-
   def show
     @title = 'Schema'
     params[:table] = params[:id]
+    @indices = clazz.connection.indexes(params[:table])
   end
 
 end
