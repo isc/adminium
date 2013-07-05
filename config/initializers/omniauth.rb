0e1ad2a2dcb07268493176a6eb92e599b63054e3
Ivan Schneider <contact@ivanschneider.fr>
2013-07-05 11:58:04 +0200

    redid store for open id, might resolve the invalid credentials bug

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
-require 'openid/store/filesystem'
-Rails.application.config.middleware.use OmniAuth::Strategies::OpenID, store: OpenID::Store::Filesystem.new('/tmp'),
+if ENV["REDISTOGO_URL"]
+  REDIS = Redis.connect url: ENV["REDISTOGO_URL"]
+else
+  REDIS = Redis.new db: (Rails.env.test? ? 3 : 0)
+end
+Rails.application.config.middleware.use OmniAuth::Strategies::OpenID, store: OpenID::Store::Redis.new(REDIS),
   name: 'google', identifier: 'https://www.google.com/accounts/o8/id'
--- config/initializers/redis.rb
+++
@@ -1,5 +0,0 @@
-if ENV["REDISTOGO_URL"]
-  REDIS = Redis.connect url: ENV["REDISTOGO_URL"]
-else
-  REDIS = Redis.new db: (Rails.env.test? ? 3 : 0)
-end
