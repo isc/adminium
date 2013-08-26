35fafe5e17eb4efa427f941d357d115b4d8e3e12
Jessy Bernal <jessy.bernal@gmail.com>
2013-08-26 14:38:50 +0200

    order apps by name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
       if current_user.heroku_provider?
         @apps = heroku_api.get_apps.data[:body].sort{|app1,app2| app1['name'] <=> app2['name']}
         account_heroku_ids = @apps.map{|app| "app#{app['id']}@heroku.com" }
-        @installed_apps = Account.where(deleted_at: nil, heroku_id: account_heroku_ids)
+        @installed_apps = Account.where(deleted_at: nil, heroku_id: account_heroku_ids).order(:name)
         installed_heroku_ids = @installed_apps.map{|account| account.heroku_id_only}
         @apps.delete_if {|app| installed_heroku_ids.include? app['id'].to_s }
       else
