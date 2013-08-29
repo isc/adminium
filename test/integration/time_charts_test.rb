436dd3b5a3d9bfc9b6593af3d42a1aab7a9b8349
Jessy Bernal <jessy.bernal@gmail.com>
2013-08-29 13:18:01 +0200

    fix test

--- test/integration/time_charts_test.rb
+++ test/integration/time_charts_test.rb
@@ -12,7 +12,7 @@ class TimeChartsTest < ActionDispatch::IntegrationTest
     2.times { FixtureFactory.new(:user, created_at: 3.days.ago) }
     FixtureFactory.new(:user, created_at: 5.days.ago)
     visit time_chart_resources_path(:users, column: 'created_at')
-    assert_match(/\[".*",1\].*\[".*",2\]/, page.find('script[type="text/javascript"]').text)
+    assert_match(/\[".*",1,.*\].*\[".*",2,".*"\]/, page.find('script[type="text/javascript"]').text)
   end
   
   test "display timechart with periodic grouping" do
@@ -20,7 +20,7 @@ class TimeChartsTest < ActionDispatch::IntegrationTest
     FixtureFactory.new(:user, created_at: (Time.now.beginning_of_week + 2.days))
     visit time_chart_resources_path(:users, column: 'created_at', grouping: 'dow')
     # FIXME because of timezones we end up with Sunday and Tuesday
-    assert_match(/\["Sunday",2\].*\["Tuesday",1\]/, page.find('script[type="text/javascript"]').text)
+    assert_equal 'chart_data = [["Sunday",2,0.0],["Tuesday",1,2.0]]', page.find('script[type="text/javascript"]').text
   end
   
 end
