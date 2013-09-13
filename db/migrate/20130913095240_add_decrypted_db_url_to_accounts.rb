1b4dbf8079e2abfaa906bdbaf34f00f9436eaaa0
Jessy Bernal <jessy.bernal@gmail.com>
2013-09-13 12:01:47 +0200

    fix

--- db/migrate/20130913095240_add_decrypted_db_url_to_accounts.rb
+++ db/migrate/20130913095240_add_decrypted_db_url_to_accounts.rb
@@ -1,7 +1,7 @@
 class AddDecryptedDbUrlToAccounts < ActiveRecord::Migration
   def change
     add_column :accounts, :decrypted_db_url, :string
-    Account.where('encrypted_db_url IS NOT NULL').find_each |account| do
+    Account.where('encrypted_db_url IS NOT NULL').find_each do |account|
       account.decrypted_db_url = account.db_url
       account.save!
     end
