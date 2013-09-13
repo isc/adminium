d416d79d24465dac3fb398cfd01bbf086a4d838d
Ivan Schneider <contact@ivanschneider.fr>
2013-09-13 11:58:42 +0200

    fix migration

--- db/migrate/20130913094925_add_iv_and_salt_for_encrypted_db_url.rb
+++ db/migrate/20130913094925_add_iv_and_salt_for_encrypted_db_url.rb
@@ -1,6 +1,6 @@
 class AddIvAndSaltForEncryptedDbUrl < ActiveRecord::Migration
   def change
-    add_column :accounts, :encrypted_password_salt, :string
-    add_column :accounts, :encrypted_password_iv, :string
+    add_column :accounts, :encrypted_db_url_salt, :string
+    add_column :accounts, :encrypted_db_url_iv, :string
   end
 end
