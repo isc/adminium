f09ff6a8647bb0e1340524f42a37c36629a3d753
Ivan Schneider <contact@ivanschneider.fr>
2013-09-13 13:59:01 +0200

    la couenne

--- db/migrate/20130913095120_reencrypt_db_urls.rb
+++ db/migrate/20130913095120_reencrypt_db_urls.rb
@@ -1,7 +1,7 @@
 class ReencryptDbUrls < ActiveRecord::Migration
   def change
     Account.reset_column_information
-    Account.where.not(encrypted_db_url: [nil, ''])find_each do |account|
+    Account.where.not(encrypted_db_url: [nil, '']).find_each do |account|
       account.db_url = account.decrypted_db_url
       account.save validate: false
     end
