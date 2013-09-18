6f7b21f94ea8d5ec5ce6f67c580d71eb2356b2e0
Jessy Bernal <jessy.bernal@gmail.com>
2013-09-18 10:28:59 +0200

    remove log_tags

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,8 +28,6 @@ Adminium::Application.configure do
   
   # Raise exception on mass assignment protection for Active Record models
   config.active_record.mass_assignment_sanitizer = :strict
-  
-  config.log_tags = [lambda {|req| req.env['ACCOUNT_ID']}]
 
   config.middleware.insert_after ActionDispatch::Static, Rack::LiveReload
 end
