d56e470b4f5107a14cc018c6232cd84fb4030427
Ivan Schneider <contact@ivanschneider.fr>
2016-01-23 20:24:43 +0100

    Client options for OAuth provider on Doctolib.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,9 @@ REDIS = Redis.new url: Figaro.env.redis_url, db: (Rails.env.test? ? 3 : 0)
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, Figaro.env.google_oauth_client_id, Figaro.env.google_oauth_client_secret,
-    {scope: ['userinfo.email'], access_type: 'online'}
+    {scope: ['userinfo.email'], access_type: 'online',
+     client_options: Figaro.env.external_tools_proxy.present? ? {connection_opts: {proxy: Figaro.env.external_tools_proxy}} : {}
+    }
 end
 
 if Figaro.env.use_heroku_omniauth
