9b81543c4be10b6ddfaa3834a5a6c7dc2b79c657
Ivan Schneider <contact@ivanschneider.fr>
2016-02-04 20:24:46 +0100

    minor refactor

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,16 +1,13 @@
 REDIS = Redis.new url: Figaro.env.redis_url, db: (Rails.env.test? ? 3 : 0)
 
 Rails.application.config.middleware.use OmniAuth::Builder do
+  client_options = Figaro.env.external_tools_proxy.present? ? {connection_opts: {proxy: Figaro.env.external_tools_proxy}} : {}
   provider :google_oauth2, Figaro.env.google_oauth_client_id, Figaro.env.google_oauth_client_secret,
-    {scope: ['userinfo.email'], access_type: 'online',
-     client_options: Figaro.env.external_tools_proxy.present? ? {connection_opts: {proxy: Figaro.env.external_tools_proxy}} : {}
-    }
+    scope: ['userinfo.email'], access_type: 'online', client_options: client_options
 end
 
 if Figaro.env.use_heroku_omniauth
   Rails.application.config.middleware.use OmniAuth::Builder do
-    oauth_id = Figaro.env.heroku_oauth_id
-    secret = Figaro.env.heroku_oauth_secret
-    provider :heroku, oauth_id, secret, scope: 'identity, write-protected'
+    provider :heroku, Figaro.env.heroku_oauth_id, Figaro.env.heroku_oauth_secret, scope: 'identity, write-protected'
   end
 end
