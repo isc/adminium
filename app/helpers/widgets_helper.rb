7d956bab46e015f2f2fbe3be91a9ca7cbe03caa0
Ivan Schneider <contact@ivanschneider.fr>
2016-04-08 19:33:07 +0200

    fix display of widgets on dashboard

--- app/helpers/widgets_helper.rb
+++ app/helpers/widgets_helper.rb
@@ -2,7 +2,7 @@ module WidgetsHelper
   def widget_query_path widget, opts = {}
     options = {asearch: widget.advanced_search.presence, order: widget.order}
     options[:widget_id] = widget.id if opts[:with_id]
-    if widget.is_a?(TableWidget) || !options[:with_id]
+    if widget.is_a?(TableWidget) || !opts[:with_id]
       resources_path widget.table, options
     else
       options[:column] = widget.columns
