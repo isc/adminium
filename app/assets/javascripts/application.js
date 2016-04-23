a673ec45323800ff6742a1573b0eb11ea4f7536d
Ivan Schneider <contact@ivanschneider.fr>
2016-04-23 14:37:07 +0200

    Update jquery csv.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,7 +6,7 @@
 //= require sh_main.min
 //= require sh_ruby.min
 //= require bootstrap-wysihtml5
-//= require jquery.csv-0.71.min
+//= require jquery.csv-0.8.1.min
 //= require select2
 //= require md5.min
 //= require bootstrap-datepicker
