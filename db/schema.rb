d7748fc10c4594851a6dc0b1ece9c086a34ea436
Ivan Schneider <contact@ivanschneider.fr>
2017-02-09 22:33:57 +0100

    Change encrypted_db_url column type to text

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160910140727) do
+ActiveRecord::Schema.define(version: 20170209213230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20160910140727) do
     t.string   "callback_url"
     t.string   "name"
     t.string   "owner_email"
-    t.string   "encrypted_db_url"
+    t.text     "encrypted_db_url"
     t.string   "adapter"
     t.datetime "created_at"
     t.datetime "updated_at"
