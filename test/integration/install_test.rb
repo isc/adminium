d491822d7b15274433facba3dde1bb6b6b2e66bb
Ivan Schneider <contact@ivanschneider.fr>
2017-10-20 19:47:43 +0200

    Should stabilize tests

--- test/integration/install_test.rb
+++ test/integration/install_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class InstallTest < ActionDispatch::IntegrationTest
+  include ActiveJob::TestHelper
+
   test 'missing db url page' do
     login
     visit setup_database_connection_install_path
@@ -14,10 +16,12 @@ class InstallTest < ActionDispatch::IntegrationTest
     InstallsController.any_instance.stubs(:heroku_api).returns stub(collaborator: stub(list: collaborators))
     page.set_rack_session user: create(:user, provider: 'heroku', email: 'j@m.com').id
     visit invite_team_install_path
-    assert_difference 'ActionMailer::Base.deliveries.length' do
-      click_button 'Send a welcome email to the team'
-      assert_no_text 'Installation steps'
-      assert_text 'Dashboard'
+    perform_enqueued_jobs do
+      assert_difference 'ActionMailer::Base.deliveries.length' do
+        click_button 'Send a welcome email to the team'
+        assert_no_text 'Installation steps'
+        assert_text 'Dashboard'
+      end
     end
     mail = ActionMailer::Base.deliveries.last
     assert_equal 'j@m.com', mail['to'].to_s
