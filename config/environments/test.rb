9a9f799ac4108b6d20503eb1385b90baa2773415
Ivan Schneider <contact@ivanschneider.fr>
2017-11-05 15:50:35 +0100

    Precompile assets before running tests on Heroku

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -40,6 +40,5 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.assets.prefix = '/capybara_test_assets'
   config.middleware.use RackSessionAccess::Middleware
 end
