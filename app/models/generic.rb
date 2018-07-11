86bb61d73202208178f31a9925147a142d1022bc
Ivan Schneider <contact@ivanschneider.fr>
2018-07-11 14:59:16 +0200

    More PG system tables

--- app/models/generic.rb
+++ app/models/generic.rb
@@ -5,7 +5,8 @@ Sequel.extension :named_timezones
 Sequel.tzinfo_disambiguator = proc {|_datetime, periods| periods.first}
 
 class Generic
-  PG_SYSTEM_TABLES = %i(pg_stat_activity pg_stat_statements pg_stat_all_indexes pg_stat_user_tables).freeze
+  PG_SYSTEM_TABLES = %i(pg_stat_activity pg_stat_statements pg_stat_all_indexes pg_stat_user_tables
+                        pg_statio_user_tables pg_statio_user_indexes).freeze
   STATEMENT_TIMEOUT = 20_000
   attr_accessor :db_name, :account_id, :db, :account
   attr_reader :current_adapter
@@ -106,7 +107,7 @@ class Generic
     return @tables if @tables
     @tables = (@db.tables + @db.views).sort - %i(ar_internal_metadata)
     @account.update_attribute :tables_count, @tables.size if @account.tables_count != @tables.size
-    @tables.concat %i(pg_stat_activity pg_stat_all_indexes pg_stat_user_tables) if postgresql?
+    @tables.concat PG_SYSTEM_TABLES - %i(pg_stat_statements) if postgresql?
     @tables
   end
 
