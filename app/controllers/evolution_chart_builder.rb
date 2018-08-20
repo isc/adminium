29ad91706e16814a63859efaf43dbeb2a6e60f09
Ivan Schneider <contact@ivanschneider.fr>
2018-08-20 14:43:07 +0200

    Fix for pg_stat_statements live evolution

--- app/controllers/evolution_chart_builder.rb
+++ app/controllers/evolution_chart_builder.rb
@@ -1,7 +1,7 @@
 module EvolutionChartBuilder
   def evolution_chart
     column = qualify params[:table], params[:column]
-    label_column = {'pg_stat_statements' => :query, 'pg_stat_all_indexes' => :indexrelname}
+    label_column = {'pg_stat_statements' => :query, 'pg_stat_all_indexes' => :indexrelname}[params[:table]]
     @items = resource.query
     dataset_filtering
     @data = @items.select(column, label_column).all.map {|row| [row[label_column], row[params[:column].to_sym]]}.to_h
