53c7e648e431858a81309ec88b7e06873a63edfc
Ivan Schneider <contact@ivanschneider.fr>
2018-10-27 13:18:15 +0200

    Restart identity when truncating fixture db
    In order to try and stabilize test screenshots.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -87,7 +87,7 @@ class FixtureFactory
   def self.clear_db
     with_fixture_connection do
       %w(users comments groups roles roles_users documents uploaded_files).each do |table_name|
-        ActiveRecord::Base.connection.execute("TRUNCATE TABLE #{table_name}")
+        ActiveRecord::Base.connection.execute("TRUNCATE TABLE #{table_name} RESTART IDENTITY")
       end
     end
   end
