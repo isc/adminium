75f772287685134e566bae27c08d22fbd1ceebb8
Ivan Schneider <contact@ivanschneider.fr>
2019-01-09 20:53:58 +0100

    test display account settings edition

--- test/integration/accounts_test.rb
+++ test/integration/accounts_test.rb
@@ -15,12 +15,27 @@ class AccountsTest < ActionDispatch::IntegrationTest
 
   test 'update account settings' do
     account = login
+    FixtureFactory.new(:user, pseudo: 'Zoé')
+    visit resources_path(:users)
+    assert_no_text 'less than a minute ago'
     visit edit_account_path
     new_db_url = "#{account.db_url}?plop=plip"
     fill_in 'Database URL', with: new_db_url
     click_button 'Update Account'
+    assert_text 'Changes saved'
     assert has_field?('Database URL', with: new_db_url)
     fill_in 'Database URL', with: 'stable screenshot'
     save_screenshot 'account_settings.png'
+    click_on 'Display settings'
+    assert has_field?('Per page', with: 25)
+    fill_in 'Per page', with: 150
+    select 'less than a minute ago'
+    click_on 'Save settings'
+    assert_text 'Changes saved'
+    click_on 'Display settings'
+    assert has_field?('Per page', with: 150)
+    assert has_select?('Datetime format', selected: 'less than a minute ago')
+    visit resources_path(:users)
+    assert_text 'less than a minute ago'
   end
 end
