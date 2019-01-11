75044e145b8c06d01524ab0d3fc8442b6a6ccb72
Ivan Schneider <contact@ivanschneider.fr>
2019-01-11 16:44:26 +0100

    handle JSONB containment search error

--- app/models/search.rb
+++ app/models/search.rb
@@ -5,15 +5,31 @@ class Search < ApplicationRecord
 
   attr_accessor :generic
 
+  private
+
   def conditions_operands
     resource = Resource::Base.new generic, table
     conditions.each do |condition|
-      next unless condition['operator'] == 'IN'
-      type = resource.column_type condition['column'].to_sym
-      next unless type == :integer
-      condition['operand'].split(',').each do |value|
-        errors.add(:base, "#{value} is not an integer") unless value.strip =~ /\A\d+\z/
-      end
+      validate_integer_array_condition condition, resource
+      validate_jsonb_containment_condition condition
+    end
+  end
+
+  def validate_integer_array_condition condition, resource
+    return unless condition['operator'] == 'IN'
+    type = resource.column_type condition['column'].to_sym
+    return unless type == :integer
+    condition['operand'].split(',').each do |value|
+      errors.add(:base, "#{value} is not an integer") unless value.strip =~ /\A\d+\z/
+    end
+  end
+
+  def validate_jsonb_containment_condition condition
+    return unless condition['type'] == 'jsonb' && condition['operator'] == 'contains'
+    begin
+      JSON.parse(condition['operand'])
+    rescue JSON::ParserError
+      errors.add :base, "#{condition['operand']} cannot be parsed as JSON"
     end
   end
 end
---
+++ test/integration/searches_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class SearchesTest < ActionDispatch::IntegrationTest
+  def setup
+    @account = login
+  end
+
+  test 'search on jsonb column' do
+    FixtureFactory.new(:role, metadata: { level: 12 })
+    FixtureFactory.new(:role, metadata: { level: 23 })
+    visit resources_path(:roles)
+    assert_text '2 records'
+    click_on 'All records'
+    click_on 'New filter'
+    select 'metadata'
+    select 'contains'
+    find('input[data-type="jsonb"]').set '{"level": 23'
+    click_on 'Search'
+    assert_text '{"level": 23 cannot be parsed as JSON'
+    click_on 'All records'
+    click_on 'New filter'
+    select 'metadata'
+    select 'contains'
+    find('input[data-type="jsonb"]').set '{"level": 23}'
+    click_on 'Search'
+    assert_text '1 record'
+  end
+end
