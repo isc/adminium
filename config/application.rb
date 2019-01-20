7842996eedc83d9dbebe0833e3c724948b20e78c
Ivan Schneider <contact@ivanschneider.fr>
2019-01-20 13:34:04 +0100

    Rails app:update

--- config/application.rb
+++ config/application.rb
@@ -10,9 +10,8 @@ require 'csv'
 module Adminium
   class Application < Rails::Application
     config.filter_parameters += %i(password db_url)
-    config.active_record.time_zone_aware_types = %i[datetime time]
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.active_record.time_zone_aware_types = %i(datetime time)
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.0
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,15 +13,17 @@ Rails.application.configure do
   config.consider_all_requests_local = true
 
   # Enable/disable caching. By default caching is disabled.
+  # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      'Cache-Control' => 'public, max-age=172800'
+      'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
+
     config.cache_store = :null_store
   end
 
@@ -39,6 +41,9 @@ Rails.application.configure do
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
+  # Highlight code that triggered database queries in logs.
+  config.active_record.verbose_query_logs = true
+
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
@@ -52,5 +57,5 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
