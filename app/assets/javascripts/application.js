d3dbd0829e0c9d5d48a945b97549f31dbc4ded5d
Ivan Schneider <contact@ivanschneider.fr>
2019-01-30 07:14:37 +0100

    getting rid of rails-assets rubygem source (#271)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,6 +9,6 @@
 //= require jquery.csv-0.8.1.min
 //= require select2
 //= require md5.min
-//= require bootstrap-datepicker
+//= require bootstrap-datepicker.min
 //= require clipboard.min
 //= require_tree .
