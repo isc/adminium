85eca04bdd855750d1a945a735b9d38826efc0d1
Ivan Schneider <contact@ivanschneider.fr>
2019-02-17 16:05:06 +0100

    add temp failure

--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class AccountTest < ActiveSupport::TestCase
   test 'track plan' do
+    assert_equal 2, 9
     account = Account.create plan: Account::Plan::ENTERPRISE
     assert_equal Account::Plan::ENTERPRISE, account.plan_migrations.last[:plan]
   end
