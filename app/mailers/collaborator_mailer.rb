de6f1039b63f32bd7496291fca7108bda08a8ff2
Ivan Schneider <contact@ivanschneider.fr>
2019-06-27 09:23:12 +0200

    Remove sendgrid tracking

--- app/mailers/collaborator_mailer.rb
+++ app/mailers/collaborator_mailer.rb
@@ -1,17 +1,12 @@
 class CollaboratorMailer < ActionMailer::Base
-  include SendGrid
-  sendgrid_enable :ganalytics, :opentrack
   default from: 'Adminium <no-reply@adminium.io>'
 
   def notify_collaboration collaborator, domain
-    sendgrid_category 'notify_collaboration'
     @collaborator, @domain = collaborator, domain
     mail to: collaborator.email, subject: "Project #{collaborator.account.name} on Adminium"
   end
 
   def welcome_heroku_collaborator emails, account, user
-    sendgrid_category 'welcome'
-    sendgrid_recipients emails
     @account = account
     @user = user
     mail to: emails, subject: "Adminium add-on has been installed on #{account.name}"
