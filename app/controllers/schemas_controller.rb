87533500e6e46b28dfaa8c22fc32e01d4b406d1e
Ivan Schneider <contact@ivanschneider.fr>
2019-08-16 17:28:19 +0200

    Fix truncate table on MySQL (#374)

--- app/controllers/schemas_controller.rb
+++ app/controllers/schemas_controller.rb
@@ -99,7 +99,11 @@ class SchemasController < ApplicationController
 
   def truncate
     if params[:id] == params[:table_name_confirmation]
-      @generic.db[Sequel.identifier params[:id]].truncate restart: params[:restart].present?
+      if @generic.postgresql?
+        @generic.db[Sequel.identifier params[:id]].truncate restart: params[:restart].present?
+      else
+        @generic.db[Sequel.identifier params[:id]].truncate
+      end
     else
       flash[:error] = 'Truncating the table failed: the table name confirmation mismatched.'
     end
