49c7c2396fbdd747514cfa40aaa6f724cb731c14
Ivan Schneider <ivan.schneider@doctolib.com>
2019-08-18 13:56:10 +0200

    Searches conditions as JSONB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_17_113526) do
+ActiveRecord::Schema.define(version: 2019_08_18_115438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,7 +75,7 @@ ActiveRecord::Schema.define(version: 2019_08_17_113526) do
     t.string "name"
     t.string "table"
     t.integer "account_id"
-    t.json "conditions"
+    t.jsonb "conditions"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["account_id"], name: "index_searches_on_account_id"
