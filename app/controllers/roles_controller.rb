0211312b3157a19fec523a93a450d1ab444d9141
Ivan Schneider <ivan.schneider@doctolib.com>
2020-02-10 18:06:47 +0100

    Log role events
    Based on work from Renaud Durand in the Doctolib clone.

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -18,6 +18,7 @@ class RolesController < ApplicationController
   def create
     @role = current_account.roles.build role_params
     if @role.save
+      log_events
       redirect_to roles_url, success: 'Role successfully created'
     else
       render :new
@@ -27,6 +28,7 @@ class RolesController < ApplicationController
   def update
     @role = current_account.roles.find params[:id]
     if @role.update role_params
+      log_events
       redirect_to roles_url, success: 'Role successfully updated'
     else
       render :new
@@ -35,13 +37,26 @@ class RolesController < ApplicationController
 
   def destroy
     @role = current_account.roles.find params[:id]
-    @role.destroy
+    @role.destroy!
+    log_events
     redirect_to roles_url, success: 'Role successfully destroyed'
   end
 
   private
 
-  def role_params
-    params.require(:role).permit(:name, collaborator_ids: [], permissions: {})
-  end
+    def log_events
+      base = { username: current_user.name, role: "#{@role.name} (id:#{@role.id})" }
+      return logger.warn base.merge(action: 'destroy-role').to_json if action_name == 'destroy'
+      if @role.saved_change_to_permissions?
+        logger.warn base.merge(action: 'update-role-permissions',
+          permissions: Hashdiff.diff(*@role.saved_changes[:permissions])).to_json
+      end
+      if role_params[:collaborator_ids].present?
+        logger.warn base.merge(action: 'update-role-members', collaborators: role_params[:collaborator_ids])
+      end
+    end
+
+    def role_params
+      params.require(:role).permit(:name, collaborator_ids: [], permissions: {})
+    end
 end
