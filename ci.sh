47e42f146b4277facbada162a40dd97f240de687
Ivan Schneider <contact@ivanschneider.fr>
2020-11-19 21:19:14 +0100

    Trying to find rake (#521)

--- ci.sh
+++ ci.sh
@@ -2,7 +2,7 @@
 
 exit_status=0
 
-bin/rake test || exit_status=1
+vendor/bundle/bin/rake test || exit_status=1
 
 argos upload tmp/capybara --token aaacbbd69f88ea80cbfe34055c2974cfc3b9ef04 \
 --branch $HEROKU_TEST_RUN_BRANCH --commit $HEROKU_TEST_RUN_COMMIT_VERSION
