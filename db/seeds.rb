30ef9438d996e469e175bddf0a81622c0039df7b
Ivan Schneider <ivan.schneider@doctolib.com>
2022-11-30 09:16:21 +0100

    Update seeds to point to adminium-development database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
-account = Account.create! name: 'doctolib-development', plan: 'complimentary',
-                          db_url: "postgresql://#{ENV['USER']}@localhost/doctolib-development"
+account = Account.create! name: 'adminium-development', plan: 'complimentary',
+                          db_url: "postgresql://#{ENV['USER']}@localhost/adminium-development"
 user = User.create! email: 'contact@ivanschneider.fr', provider: 'google_oauth2', name: 'Ivan Schneider'
 Collaborator.create! user: user, account: account, email: user.email, is_administrator: true, kind: user.provider
