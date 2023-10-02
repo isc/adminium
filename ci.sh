bcc2239e70ff989054843cf6ce5f71a192c153b0
Ivan Schneider <ivan.schneider@doctolib.com>
2023-10-02 14:24:35 +0200

    Update Argos screenshots

--- ci.sh
+++ ci.sh
@@ -4,8 +4,8 @@ exit_status=0
 
 vendor/bundle/bin/rake test || exit_status=1
 
-argos upload tmp/capybara --token aaacbbd69f88ea80cbfe34055c2974cfc3b9ef04 \
---branch $HEROKU_TEST_RUN_BRANCH --commit $HEROKU_TEST_RUN_COMMIT_VERSION
+argos upload tmp/capybara --token $ARGOS_TOKEN \
+--reference-branch $HEROKU_TEST_RUN_BRANCH --reference-commit $HEROKU_TEST_RUN_COMMIT_VERSION
 
 curl --request PATCH "${REMOTE_REPORTER_URL}/${HEROKU_TEST_RUN_ID}?node_total=${CI_NODE_TOTAL}"
 
