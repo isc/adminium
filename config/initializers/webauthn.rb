aafd1cd5760aeff43458fd33297effcc884bdb58
Ivan Schneider <ivan.schneider@doctolib.com>
2023-10-13 10:49:01 +0200

    Remove unnecessary initializer

--- config/initializers/webauthn.rb
+++
@@ -1,5 +0,0 @@
-WebAuthn.configure do |config|
-    config.origin = ENV.fetch("APP_URL", "http://localhost:3000")
-    config.rp_name = "Adminium"
-    config.credential_options_timeout = 120_000
-  end
